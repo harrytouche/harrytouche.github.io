4bf385f3b8846802aea423c21c257efc53a272d0
harrytouche <harrytouche@gmail.com>
2018-02-12 10:52:26 +0000

    AAM sendSignal

--- test-site/scripts.js
+++ test-site/scripts.js
@@ -12,7 +12,19 @@ document.title = digitalData.pageName
 
 
 
-
-
-
-
+// send signal off to AAM and print out the response
+sendSignal = function(dilObject, inputObject){
+  dilObject.api
+  .signals(inputObject, 'c_')
+  .afterResult(function(responseBody){
+    var outputStatement = []
+    outputStatement.push("UUID: " + responseBody.uuid)
+    outputStatement.push("---------Segments----------")
+    for(var i=0; i<responseBody.stuff.length; i++){
+      outputStatement.push(responseBody.stuff[i].cn + " ---> " + responseBody.stuff[i].cv)
+    }
+    outputStatement.push("-------End Segments--------")
+    console.log(outputStatement.join("\n"))
+  })
+  .submit();
+}
